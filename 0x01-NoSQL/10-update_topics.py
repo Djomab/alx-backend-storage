d6de5b863d2d2c30bb785f57fc2a6c14a3401707
Djogona Mahamat <djogonamahamatbelna@gmail.com>
2023-01-22 01:35:10 -0800

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""pymong update documents"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    query = {"name": name}
+    newValues = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, newValues)
